2ebb06670e4a6155e2803bb1e6e33a1421eda1f9
miskam-gg <143910099+miskam-gg@users.noreply.github.com>
2024-01-26 02:32:13 +0300

    12

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import news_list, news_detail
+from .views import news_list, news_detail, news_search, PostCreateView, PostUpdateView, PostDeleteView
+
+app_name = 'news'
 
 urlpatterns = [
     path('news/', news_list, name='news_list'),
     path('news/<int:post_id>/', news_detail, name='news_detail'),
+    path('news/search/', news_search, name='news_search'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,12 +1,49 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .filters import PostFilter
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'news/post_form.html'
+    success_url = reverse_lazy('news:news_list')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'news/post_form.html'
+    success_url = reverse_lazy('news:news_list')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'news/post_confirm_delete.html'
+    success_url = reverse_lazy('news:news_list')
 
 def news_list(request):
     news_list = Post.objects.filter(post_type='news').order_by('-created_at')
-    context = {'news_list': news_list}
+    paginator = Paginator(news_list, 10)
+
+    page = request.GET.get('page')
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+
+    context = {'news': news}
     return render(request, 'news/news_list.html', context)
 
 def news_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     context = {'post': post}
     return render(request, 'news/news_detail.html', context)
+
+def news_search(request):
+    filter = PostFilter(request.GET, queryset=Post.objects.all())
+    context = {'filter': filter}
+    return render(request, 'news/news_search.html', context)
